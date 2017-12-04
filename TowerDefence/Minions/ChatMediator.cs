d7f9a650eb280ab0fb7db5aa427c2741e764afaf
Vilius Turenko <vilius.turenko@ktu.edu>
2017-12-04 19:59:38 +0200

    Pridėti mediatoriaus failai

---
+++ TowerDefence/Minions/ChatMediator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefence.Minions
+{
+   public abstract class ChatMediator
+    {
+        public abstract void sendMessage(AbstractCloneableMinion sender, string msg);
+        public abstract void addMinion(AbstractCloneableMinion minion);
+    }
+}
---
+++ TowerDefence/Minions/ChatRoom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefence.Minions
+{
+    public class ChatRoom : ChatMediator
+    {
+        MinionRepository minions=new MinionRepository();
+        public override void addMinion(AbstractCloneableMinion minion)
+        {
+            minions.Add(minion);
+        }
+
+        public override void sendMessage(AbstractCloneableMinion sender, string msg)
+        {
+            for (Iterator iter = minions.getIterator(); iter.hasNext();)
+            {
+                AbstractCloneableMinion m = (AbstractCloneableMinion)iter.next();
+              
+                m.receiveMessage(msg);
+            }
+        }
+
+    }
+}
